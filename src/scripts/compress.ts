f1690bbf9940f498ca762b8589fdd615f7bbc9f4
Ricardo Badillo <ricardo.badillo@urbe.edu>
2024-09-17 16:50:20 -0400

    Añadida dependencia para la consideración de imágenes con formato jpg e integración de lógica para crear redimensiones específicas

--- src/scripts/compress.ts
+++ src/scripts/compress.ts
@@ -1,23 +1,26 @@
-import imagemin from "imagemin";
-import imageminPngquant from "imagemin-pngquant";
-import imageminWebp from "imagemin-webp";
+import imagemin from 'imagemin';
+import imageminJpegRecompress from 'imagemin-jpeg-recompress';
+import imageminPngquant from 'imagemin-pngquant';
+import imageminWebp from 'imagemin-webp';
+
 import { dirname, resolve } from 'path';
 import { fileURLToPath } from 'url';
 
-import { buildLogger } from "../plugins/logger.plugin.js";
+import { buildLogger } from '../plugins/logger.plugin.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-const directory = resolve(__dirname, '../assets/images/*.{jpg,png}');
+const directory = resolve(__dirname, '../assets/images/*.{jpg,jpeg,png}');
 const logger = buildLogger('compress.js');
 
-const pngPlugin = imageminPngquant({ quality: [ 0.8, 0.8 ] });
+const jpgPlugin = imageminJpegRecompress({ quality: 'high' });
+const pngPlugin = imageminPngquant({ quality: [0.8, 0.8] });
 const webpPlugin = imageminWebp({ quality: 80 });
 
 (async () => {
-  const files = await imagemin([ directory ], {
+  const files = await imagemin([directory], {
     destination: resolve(__dirname, '../assets/images/'),
-    plugins: [ pngPlugin, webpPlugin ],
+    plugins: [jpgPlugin, pngPlugin, webpPlugin],
   });
 
   if (files.length > 0) {
--- src/scripts/resize.ts
+++ src/scripts/resize.ts
@@ -10,19 +10,27 @@ const __dirname = dirname(__filename);
 const directory = resolve(__dirname, '../assets/images');
 const logger = buildLogger('compress.js');
 
-readdirSync(directory).forEach(async (file: string) => {
+const imageSizes = [
+  { name: 'desktop', size: 1600 },
+  { name: 'laptop', size: 1024 },
+  { name: 'tablet', size: 768 },
+  { name: 'mobile', size: 320 },
+];
 
-  if (file.includes('small') || file.includes('medium') || file.includes('large')) return;
+readdirSync(directory).forEach(async (file: string) => {
+  if (['mobile', 'tablet', 'laptop', 'desktop'].includes(file)) return;
 
   try {
-    const image = sharp(`${ directory }/${ file }`);
+    const image = sharp(`${directory}/${file}`);
     const name = file.split('.')[0];
-    const { format } = await image.metadata();
+    const { format, width } = await image.metadata();
 
-    if (!name.includes('small') && !name.includes('medium') && !name.includes('large') && (format === 'png' || format === 'webp')) {
-      image.resize(450).toFile(`${ directory }/${ name }-small.${ format }`);
-      image.resize(750).toFile(`${ directory }/${ name }-medium.${ format }`);
-      image.resize(1800).toFile(`${ directory }/${ name }-large.${ format }`);
+    if (!format) return logger.log('No se pudo obtener el formato del archivo');
+
+    for (let index = 0; index < imageSizes.length; index++) {
+      if (width! < imageSizes[index].size) continue;
+
+      image.resize(imageSizes[index].size).toFile(`${directory}/${name}-${imageSizes[index].name}.${format}`);
     }
 
     logger.log('Los archivos han sido redimensionados satisfactoriamente');
